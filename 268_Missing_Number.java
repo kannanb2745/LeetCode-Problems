8e7949d525d81ea1bae7c947342b6f5063109f9d
kannanb2745 <kannan.b2745@gmail.com>
2025-01-27 20:20:42 +0530

    feat 268

---
+++ 268_Missing_Number.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int temp =0;
+        for(int i=0; i<nums.length; i++) temp+=nums[i];
+        return ((nums.length) * (nums.length + 1) / 2) - temp;
+    }
+}
