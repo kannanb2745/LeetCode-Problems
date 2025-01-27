37a9c54e4303229d9f2c613a6635022a993f2364
kannanb2745 <kannan.b2745@gmail.com>
2025-01-27 19:22:59 +0530

    left and rigth diffrence java

---
+++ Left_Rigth_sum_difference.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+     int left =0, rigth =0;
+     int[] ans = new int[nums.length];
+     for(int i=0; i<nums.length; i++) rigth+=nums[i];
+     for(int j=0; j<nums.length; j++){
+        if(j ==0){
+            rigth-=nums[j];
+            ans[j] = Math.abs(left-rigth);
+        }
+        else{
+            left+=nums[j-1];
+            rigth-=nums[j];
+            ans[j] = Math.abs(left-rigth);
+        }
+     }   
+    return ans;
+    }
+}
