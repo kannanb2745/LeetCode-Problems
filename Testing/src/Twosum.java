dd215841f41c596bc1f6cb6a8f2310393da49b3b
kannanb2745 <kannan.b2745@gmail.com>
2025-01-29 09:51:34 +0530

    feat 217 py

---
+++ Testing/src/Twosum.java
@@ -0,0 +1,10 @@
+public class Twosum {
+    public static void main(String[] args) {
+        String a = "";
+        String[] b = {"a","a","a","a","a","a","a","a","a"};
+        for(String i : b){
+            a= a+i;
+        }
+        System.out.println(a);
+    }
+}
