c1252f21630c9cfe5ceaf41deaf73d273dcb3b5c
kannanb2745 <kannan.b2745@gmail.com>
2025-01-29 09:42:17 +0530

    feat college

--- Testing/src/Checksortedarray.java
+++ Testing/src/Checksortedarray.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class Main {
+public class Checksortedarray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the size of the arr");
@@ -23,3 +22,4 @@ public class Main {
         return true;
     }
 }
+
