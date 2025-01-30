8ac07838d3dfbf24e7192228974233593f47573a
kannanb2745 <kannan.b2745@gmail.com>
2025-01-30 09:01:02 +0530

    feat collge

--- College_Problems/divided_by.java
+++ College_Problems/divided_by.java
@@ -4,7 +4,9 @@ public class divided_by {
     public static void main(String[] args) {
         System.out.println("Enter a Number to check factor of 27");
         Scanner scanner = new Scanner(System.in);
-        int divident = scanner.nextInt();
-        System.out.println((divident%27) == 0 ? "It is divisible by 27" : "It is Not divisible by 27");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+//        System.out.println((divident%27) == 0 ? "It is divisible by 27" : "It is Not divisible by 27");
+        System.out.println(a==b ? "Equal" : a>b ? "A is Bigger" : "B is Bigger");
     }
 }
